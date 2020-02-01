cb534e633613fdc28507674dfda5ce93e9442783
Jesús Leganés-Combarro 'piranna <piranna@gmail.com>
2020-02-01 07:09:11 +0100

    Clean-up

--- __tests__/parseArgs.js
+++ __tests__/parseArgs.js
@@ -1,11 +1,10 @@
 const parseArgs = require("../lib/parseArgs");
 
 
-const CLI_PATH = process.cwd()
-
+const CLI_PATH = process.cwd();
 
 test("no arguments", function() {
-  const result = parseArgs({argv: []});
+  const result = parseArgs({ argv: [] });
 
   expect(result).toMatchInlineSnapshot(`
     Object {
@@ -19,7 +18,9 @@ test("no arguments", function() {
 });
 
 test("plugin", function() {
-  const result = parseArgs({argv: ["--plugin", "./__fixtures__/dumb plugin.js"]});
+  const result = parseArgs({
+    argv: ["--plugin", "./__fixtures__/dumb plugin.js"]
+  });
 
   expect(result).toMatchInlineSnapshot(`
     Object {
@@ -39,12 +40,9 @@ test("plugin", function() {
 });
 
 test("rule", function() {
-  const result = parseArgs({argv: [
-    "--plugin",
-    "./__fixtures__/dumb plugin 2.js",
-    "--rule",
-    "dumb rule"
-  ]});
+  const result = parseArgs({
+    argv: ["--plugin", "./__fixtures__/dumb plugin 2.js", "--rule", "dumb rule"]
+  });
 
   expect(result).toMatchInlineSnapshot(`
     Object {
@@ -66,12 +64,14 @@ test("rule", function() {
 });
 
 test("config", function() {
-  const result = parseArgs({argv: [
-    "--plugin",
-    "./__fixtures__/dumb plugin 2.js",
-    "--config",
-    "./__fixtures__/dumb 2.json:default"
-  ]});
+  const result = parseArgs({
+    argv: [
+      "--plugin",
+      "./__fixtures__/dumb plugin 2.js",
+      "--config",
+      "./__fixtures__/dumb 2.json:default"
+    ]
+  });
 
   expect(result).toMatchInlineSnapshot(`
     Object {
@@ -96,10 +96,10 @@ test("config", function() {
 });
 
 describe("resolvePackage", function() {
-  const paths = [`${CLI_PATH}/__fixtures__`]
+  const paths = [`${CLI_PATH}/__fixtures__`];
 
   test("prefixed", function() {
-    const result = parseArgs({argv: ["--plugin", "dumb"], paths});
+    const result = parseArgs({ argv: ["--plugin", "dumb"], paths });
 
     expect(result).toMatchInlineSnapshot(`
       Object {
@@ -119,7 +119,7 @@ describe("resolvePackage", function() {
   });
 
   test("scoped", function() {
-    const result = parseArgs({argv: ["--plugin", "dumb 2"], paths});
+    const result = parseArgs({ argv: ["--plugin", "dumb 2"], paths });
 
     expect(result).toMatchInlineSnapshot(`
       Object {
