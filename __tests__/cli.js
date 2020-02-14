46c296a2acc4bf21bdab9db0fbabe1143d2c5b90
Jesús Leganés-Combarro 'piranna <piranna@gmail.com>
2020-02-14 14:26:49 +0100

    Updated `tasktree-cli` & basic test for `tasktree` reporter

--- __tests__/cli.js
+++ __tests__/cli.js
@@ -69,4 +69,38 @@ describe("reports", function() {
 
     cli(rules, config, "ndjson", { stdout });
   });
+
+  test("tasktree", function() {
+    const rules = {
+      dumb: {
+        evaluate() {}
+      }
+    };
+
+    // const config = ["dumb"];
+    const config = {
+      dumb: "warning"
+    };
+
+    const buffer = [];
+    const stdout = {
+      write(data) {
+        buffer.push(data);
+      }
+    };
+
+    return cli(rules, config, "tasktree", { stdout }).then(function() {
+      expect(buffer).toMatchInlineSnapshot(`
+        Array [
+          "[?25l",
+          "  [38;2;66;133;244m❯[39m /home/piranna/github/projectlint/cli
+        [38;2;131;133;132m  [38;2;46;46;46m›[39m[38;2;131;133;132m [38;2;0;200;81m✔[39m[38;2;131;133;132m dumb[39m
+        ",
+          "[?25h",
+          "[?25l",
+          "[?25h",
+        ]
+      `);
+    });
+  });
 });
